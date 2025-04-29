50c35615cdd7b0650ddb6b4af6a2e598e7c6049a
O23216 <O23216@lab.aca>
2025-04-28 21:45:05 -0500

    entrega final

---
+++ pregunta2/pregunta2.js
@@ -0,0 +1,10 @@
+let agregarImagen = function() {
+    
+}
+
+let main = function() {
+    let butAgregar = document.getElementById("but_Agregar")
+    butAgregar.addEventListener("click", agregarImagen)
+}
+
+main()
